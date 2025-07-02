b168854a6b596e20d201050114a23063d560c506
Preet Jaiswal <preetj886655@gmail.com>
2025-07-02 16:01:46 +0530

    Delete updateCartValue.js

--- updateCartValue.js
+++
@@ -1,4 +0,0 @@
-const cartValue=document.querySelector("#cartvalue")
-export const updateCartValue=(cartProduct)=>{
-    return (cartValue.innerHTML=`<i class="fa-solid fa-cart-shopping">${cartProduct.length}</i>`)
-}
